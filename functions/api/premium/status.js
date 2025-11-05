076a5adc60b0b446a4eb7b5d71cdc4b97c64b009
DevTim99 <100614215+TizOfficial@users.noreply.github.com>
2025-11-05 16:14:36 +0100

    Delete functions/api directory

--- functions/api/premium/status.js
+++
@@ -1,8 +0,0 @@
-export const onRequestGet = async ({ request, env }) => {
-  const url = new URL(request.url);
-  const discord_id = url.searchParams.get("discord_id");
-  const active = (await env.BLAZE_PREMIUM.get(`ACTIVE:${discord_id}`)) === "1";
-  return new Response(JSON.stringify({ active }), {
-    headers: { "content-type": "application/json" }
-  });
-};
