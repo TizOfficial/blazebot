bce3c7c29afbdb3590a1132a0208de1d5d2c81e1
DevTim99 <100614215+TizOfficial@users.noreply.github.com>
2025-11-05 15:12:55 +0100

    Create status.ts

---
+++ functions/api/premium/status.ts
@@ -0,0 +1,6 @@
+export const onRequestGet = async ({ request, env }) => {
+  const url = new URL(request.url);
+  const discord_id = url.searchParams.get("discord_id");
+  const active = (await env.BLAZE_PREMIUM.get(`ACTIVE:${discord_id}`)) === "1";
+  return new Response(JSON.stringify({ active }), { headers: { "content-type": "application/json" } });
+};
